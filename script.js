53056caffcf6ee42f6f6f5af200f29ecd8929cbc
Pedro Silva do Nascimento <phpedrojs@gmail.com>
2024-07-24 15:43:13 -0300

    up

--- script.js
+++ script.js
@@ -1,41 +1,88 @@
-function load() {
-    var link1 = document.getElementById('link1');
-    var link2 = document.getElementById('link2');
-    var link3 = document.getElementById('link3');
+// Menu mobile responsivo
+class MobileNavbar {
+    constructor(mobileMenu, navList, navLinks) {
+        this.mobileMenu = document.querySelector(mobileMenu);
+        this.navList = document.querySelector(navList);
+        this.navLinks = document.querySelector(navLinks);
+        this.activeClass = 'active';
+        this.handleClick = this.handleClick.bind(this);
+        this.handleOutsideClick = this.handleOutsideClick.bind(this);
+    };
 
-    var border = document.getElementsByTagName('header')[0]
+    handleClick() {
+        this.navList.classList.toggle(this.activeClass);
+        this.mobileMenu.classList.toggle(this.activeClass);
 
-    // Styles 
-    setTimeout(function() {
-        border.style.borderBottom = '2px solid var(--cor2)';
-        border.style.transitionDuration = '.8s';
-    }, 1000);
+        if (this.navList.classList.contains(this.activeClass)) {
+            document.addEventListener('click', this.handleOutsideClick);
+        } else {
+            document.removeEventListener('click', this.handleOutsideClick);
+        };
+    };
 
-    setTimeout(function() {
-        link1.style.font = 'normal normal 27px var(--fonte3)';
-        link1.style.color = 'var(--cor3)';
-        link1.style.transitionDuration = '.3s';
-        setTimeout(function() {
-            link1.style.font = '';
-            link1.style.color = '';
-        }, 1600);
-    }, 1000);
-    setTimeout(function() {
-        link2.style.font = 'normal normal 27px var(--fonte3)';
-        link2.style.color = 'var(--cor3)';
-        link2.style.transitionDuration = '.3s';
-        setTimeout(function() {
-            link2.style.font = '';
-            link2.style.color = '';
-        }, 1600);
-    }, 2500);
-    setTimeout(function() {
-        link3.style.font = 'normal normal 27px var(--fonte3)';
-        link3.style.color = 'var(--cor3)';
-        link3.style.transitionDuration = '.3s';
-        setTimeout(function() {
-            link3.style.font = '';
-            link3.style.color = '';
-        }, 2000);
-    }, 4000);
+    handleOutsideClick(event) {
+        if (!this.navList.contains(event.target) && !this.mobileMenu.contains(event.target)) {
+            this.navList.classList.remove(this.activeClass);
+            this.mobileMenu.classList.remove(this.activeClass);
+            document.removeEventListener('click', this.handleOutsideClick);
+        };
+    };
+
+    addClickEvent() {
+        this.mobileMenu.addEventListener('click', this.handleClick);
+    };
+    
+    init() {
+        if (this.mobileMenu) {
+            this.addClickEvent();
+        };
+        return this;
+    };
 };
+
+const mobileNavbar = new MobileNavbar (
+    ".menu-btn",
+    ".menu-list", 
+    ".menu-list li",
+);
+mobileNavbar.init();
+
+
+// Scroll automatico
+const sobre = document.querySelector('.sobremin');
+function scrollSobre() {
+    event.preventDefault();
+    sobre.scrollIntoView({ behavior:'smooth' });
+};
+
+// Scroll projetos
+const proje = document.querySelector('.projetos');
+function scrollProjeto() {
+    event.preventDefault();
+    proje.scrollIntoView({ behavior:'smooth' });
+};
+
+// Scroll habilidades
+const habi = document.querySelector('.habilidades');
+function scrollHabilidade() {
+    event.preventDefault();
+    habi.scrollIntoView({ behavior:'smooth' });
+};
+
+// Scroll para voltar
+const btnTopo = document.querySelector('#btn-topo');
+window.onscroll = function ativarBotao() {
+    // Ativando botão de voltar
+    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+        btnTopo.style.display = 'block';
+    } else {
+        btnTopo.style.display = 'none';
+    };
+};
+btnTopo.addEventListener('click', function() {
+    event.preventDefault();
+    scrollTo({
+        top: 0,
+        behavior:'smooth'
+    });
+});
